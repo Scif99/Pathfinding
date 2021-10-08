6ee72c0cb38820760c0b880a84144c24a40ca890
Soleh Cifonelli <scifonelli1@yahoo.com>
2021-10-08 18:38:45 +0100

    dijkstra

---
+++ Dijkstra.cpp
@@ -0,0 +1,18 @@
+#include "implementation.cpp"
+
+/*
+From BFS to Dijkstra:
+- Graph is now weighted
+- Notion of shortest distance changes slightly - It is now the smallest total cost from the source
+- Because of this, we need to process nodes in a different fashion to get shortest distance. 'FIFO' is not enough.
+
+*/
+
+
+template<typename Location, typename Graph>
+void Dijkstra(Graph G, Location source, Location goal, ) {
+
+
+
+
+}
