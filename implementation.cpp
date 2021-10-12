6e47133a0bb9a78c7f9b066339f2feebae4c5496
Soleh Cifonelli <scifonelli1@yahoo.com>
2021-10-12 17:02:48 +0100

    Back up

--- implementation.cpp
+++ implementation.cpp
@@ -120,7 +120,7 @@ void draw_grid(const Graph& graph,
   for (int y = 0; y != graph.height; ++y) {
     for (int x = 0; x != graph.width; ++x) {
       GridLocation id {x, y};
-      if (graph.walls.find(id) != graph.walls.end()) { //check for a wall
+      if (graph.walls.find(id) != graph.walls.end()) { // first check if cell is a wall
         std::cout << std::string(field_width, '#');
       } else if (start && id == *start) { // check for the source node
         std::cout << " A ";
@@ -168,6 +168,8 @@ SquareGrid make_diagram1() {
   Now With Weights
 -------------------------------------------
 */
+
+// A grid of forest tiles, with uniform weight of 5
 struct GridWithWeights: SquareGrid {
   std::unordered_set<GridLocation> forests;
   GridWithWeights(int w, int h): SquareGrid(w, h) {}
@@ -192,110 +194,7 @@ GridWithWeights make_diagram4() {
   return grid;
 }
 
-template<typename T, typename priority_t>
-struct PriorityQueue {
-  typedef std::pair<priority_t, T> PQElement;
-  std::priority_queue<PQElement, std::vector<PQElement>,
-                 std::greater<PQElement>> elements;
-
-  inline bool empty() const {
-     return elements.empty();
-  }
-
-  inline void put(T item, priority_t priority) {
-    elements.emplace(priority, item);
-  }
-
-  T get() {
-    T best_item = elements.top().second;
-    elements.pop();
-    return best_item;
-  }
-};
-
-template<typename Location, typename Graph>
-void dijkstra_search
-  (Graph graph,
-   Location start,
-   Location goal,
-   std::unordered_map<Location, Location>& came_from,
-   std::unordered_map<Location, double>& cost_so_far)
-{
-  PriorityQueue<Location, double> frontier;
-  frontier.put(start, 0);
-
-  came_from[start] = start;
-  cost_so_far[start] = 0;
-  
-  while (!frontier.empty()) {
-    Location current = frontier.get();
-
-    if (current == goal) {
-      break;
-    }
-
-    for (Location next : graph.neighbors(current)) {
-      double new_cost = cost_so_far[current] + graph.cost(current, next);
-      if (cost_so_far.find(next) == cost_so_far.end()
-          || new_cost < cost_so_far[next]) {
-        cost_so_far[next] = new_cost;
-        came_from[next] = current;
-        frontier.put(next, new_cost);
-      }
-    }
-  }
-}
-
-template<typename Location>
-std::vector<Location> reconstruct_path(
-   Location start, Location goal,
-   std::unordered_map<Location, Location> came_from
-) {
-  std::vector<Location> path;
-  Location current = goal;
-  while (current != start) {
-    path.push_back(current);
-    current = came_from[current];
-  }
-  path.push_back(start); // optional
-  std::reverse(path.begin(), path.end());
-  return path;
-}
-
 inline double heuristic(GridLocation a, GridLocation b) {
   return std::abs(a.x - b.x) + std::abs(a.y - b.y);
 }
 
-template<typename Location, typename Graph>
-void a_star_search
-  (Graph graph,
-   Location start,
-   Location goal,
-   std::unordered_map<Location, Location>& came_from,
-   std::unordered_map<Location, double>& cost_so_far)
-{
-  PriorityQueue<Location, double> frontier;
-  frontier.put(start, 0);
-
-  came_from[start] = start;
-  cost_so_far[start] = 0;
-  
-  while (!frontier.empty()) {
-    Location current = frontier.get();
-
-    if (current == goal) {
-      break;
-    }
-
-    for (Location next : graph.neighbors(current)) {
-      double new_cost = cost_so_far[current] + graph.cost(current, next);
-      if (cost_so_far.find(next) == cost_so_far.end()
-          || new_cost < cost_so_far[next]) {
-        cost_so_far[next] = new_cost;
-        double priority = new_cost + heuristic(next, goal);
-        frontier.put(next, priority);
-        came_from[next] = current;
-      }
-    }
-  }
-}
